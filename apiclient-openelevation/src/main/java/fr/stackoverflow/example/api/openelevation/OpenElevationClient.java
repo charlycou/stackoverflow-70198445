88e9232cd29cb800822f5de9305ca0556e126206
xerx593 <alexlutz79@gmail.com>
2021-12-17 11:37:15 +0100

    move(s) and fix(es);)

--- apiclient-openelevation/src/main/java/fr/stackoverflow/example/api/openelevation/OpenElevationClient.java
+++ apiclient-openelevation/src/main/java/fr/stackoverflow/example/api/openelevation/OpenElevationClient.java
@@ -3,9 +3,7 @@ package fr.stackoverflow.example.api.openelevation;
 
 
 import fr.stackoverflow.example.api.openelevation.model.LocationElevationResultsDTO;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 public interface OpenElevationClient {
     LocationElevationResultsDTO getElevation(String locations);
 }
